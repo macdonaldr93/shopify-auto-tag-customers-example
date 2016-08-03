95af58e15e6adcddb01e512440ceaf9e29c76827
Ryan Macdonald <ryan.macdonald@shopify.com>
2016-08-03 15:57:57 -0400

    consolidate shopify resources

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 var express = require('express');
-var session = require('express-session');
 var compression = require('compression');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var logger = require('morgan');
+var morgan = require('morgan');
 var config = require('./config/env.js'); // eslint-disable-line node/no-unpublished-require
-var api = require('./lib/api.js');
 var shopify = require('./lib/shopify.js');
 
 // Build authenticated URL based on info provided in config and env
@@ -13,21 +10,18 @@ var storeAuth = 'https://' + config.apiKey + ':' + config.password + '@' + confi
 
 // Setup express server
 var app = express();
-app.use(logger('dev'));
+app.use(morgan('dev'));
 app.use(compression());
+
 // Verify webhook's origin
 app.use(bodyParser.json({
   verify: shopify.validateSignature
 }));
 app.use(bodyParser.urlencoded({
-  extended: false
+  verify: shopify.validateSignature,
+  extended: true
 }));
-app.use(cookieParser());
-app.use(session({
-  secret: 'dangerz0ne',
-  resave: false,
-  saveUninitialized: true
-}));
+
 // Block invalid webhook requests
 app.use(shopify.requireValidSignature);
 
@@ -36,7 +30,7 @@ app.post('/order_create', function(req, res) {
   // Verify conditions to take action
   if (req.body.customer.id && req.body.shipping_address.country_code === 'CA') {
     // Update customer
-    api.updateCustomerTags(storeAuth, req.body.customer.id, ['Canada', 'North America']);
+    shopify.updateCustomerTags(storeAuth, req.body.customer.id, ['Canada', 'North America']);
   }
 
   // Return OK to Shopify
